0c75506c27099c7a6018fb39d73e1098e0aa9f7f
Blazka Blatnik <bb3172@student.uni-lj.si>
2018-12-21 12:10:54 +0100

    navbar and register

--- src/app/shared/directives/navbar/navbar.ctrl.js
+++ src/app/shared/directives/navbar/navbar.ctrl.js
@@ -3,9 +3,12 @@
   var navbarCtrl = function($window) {
     var vm = this;
 
-    vm.logedIn = $window.localStorage['logedIn'];
+    vm.logedIn = $window.localStorage['logedIn'] == "true";
     vm.name = $window.localStorage['name'];
     vm.surname = $window.localStorage['surname'];
+
+
+    console.log("vm.logedIn ",vm.logedIn)
 
     vm.logout = function() {
       vm.logedIn = false;
