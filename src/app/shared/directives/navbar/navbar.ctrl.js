0264e2ae49a3785e92858413150e87277ebc46d4
Martin <martin.cebular@gmail.com>
2019-01-06 15:17:19 +0100

    Implemented playlists on profile; added library link to navbar; some styling changes

--- src/app/shared/directives/navbar/navbar.ctrl.js
+++ src/app/shared/directives/navbar/navbar.ctrl.js
@@ -6,15 +6,16 @@
     vm.logedIn = $window.localStorage['logedIn'] == "true";
     vm.name = $window.localStorage['name'];
     vm.surname = $window.localStorage['surname'];
+    vm.userId = $window.localStorage['userId'];
 
-    console.log("vm.logedIn ",vm.logedIn)
+    console.log("vm.logedIn ",vm.logedIn);
     vm.profilePictureNum = Math.floor(Math.random() * Math.floor(10))+1;
 
     vm.logout = function() {
       vm.logedIn = false;
       $window.localStorage['logedIn'] = false;
       $location.path("/");
-    }
+    };
   };
 
   angular
